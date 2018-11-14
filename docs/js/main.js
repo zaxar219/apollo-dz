4cb8a38f54cacdcd51942a3f21dfb81fc77cebe1
Vladimir.Zaharov <zaxar219@mail.ru>
2018-11-14 14:29:35 +0300

    dz-alollo-2

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,23 @@
+
 $(document).ready(function(){
-	console.log("Hello world!")
-	
-	
-	
+	// Скрипт для показа и скрытия выпадающего меню на смартфонах
+	//Создали переменные
+	var pull = $("#navigation-toggle");
+	var nav = $(".navigation ul");
+	//Описание событие при нажатии на ссылку
+	$(pull).on("click",function(e){
+		//Отменяем стандартное повидение ссылки в браузере
+		e.preventDefault();
+		//Открываем/Скрываем меню
+		$(nav).slideToggle();
+		
+		//Добовляем модификатор --activ
+		$(this).toggleClass('navigation__toggle-button--active');
+	});
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 720 && nav.is(':hidden')) {
+			nav.removeAttr('style');
+		}
+	});
 });
